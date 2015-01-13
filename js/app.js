acc2ea5b65b53d21b03e0589381d8aa5dcb5ad2f
hoovercj <hoovercj@iastate.edu>
2015-01-13 19:20:31 +0100

    Added search capabilities

--- js/app.js
+++ js/app.js
@@ -7,3 +7,33 @@
 $(document).ready(function() {
     $("img").unveil(50);
 });
+
+$("tbody").searcher({
+    itemSelector: "tr",
+    textSelector: "td",
+    inputSelector: "#searchinput",
+    toggle: function(item, containsText) {
+        // use a typically jQuery effect instead of simply showing/hiding the item element
+        if (containsText) {            
+        	$(item).fadeIn();
+        } else {            
+        	$(item).fadeOut();
+        }
+    }
+});
+
+$("#main-container").searcher({
+    itemSelector: ".section",
+    textSelector: "td",
+    inputSelector: "#searchinput",
+    toggle: function(item, containsText) {
+        if (window.console) console.log(containsText + ' ' + item);
+        // use a typically jQuery effect instead of simply showing/hiding the item element
+        if (containsText) {            
+        	$(item).fadeIn();
+        } else {            
+        	$(item).fadeOut();
+        }
+
+    }
+});
