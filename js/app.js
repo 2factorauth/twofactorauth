0bccef1ff08d7e2f56d62a28cfd231280e4f7791
psgs <fpigerre@gmail.com>
2016-01-01 17:45:40 +1000

    Enhance selection of correct category/table

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
 (function (root, $) {
-    $('.menu .dropdown').dropdown();
-    $('span.popup.exception').popup({
-    	hoverable: true
-    });
-    $('a.popup.exception').popup();
+  $('.menu .dropdown').dropdown();
+  $('span.popup.exception').popup({
+    hoverable: true
+  });
+  $('a.popup.exception').popup();
 }(window, jQuery));
 
-$(document).ready(function() {
-    $("img").unveil(50);
+$(document).ready(function () {
+  $("img").unveil(50);
+});
+
+function getStyle(element, styleProp) {
+  var validElement = document.getElementById(element),
+    validStyle;
+  if (validElement.currentStyle)
+    validStyle = validElement.currentStyle[styleProp];
+  else if (window.getComputedStyle)
+    validStyle = document.defaultView.getComputedStyle(validElement, null).getPropertyValue(styleProp);
+  return validStyle;
+}
+
+$('.category').click(function () {
+  console.log($(this).find('.website-table').css('display'));
+  if ($(this).find('.website-table').css('display') == 'block') {
+    $(this).find('.website-table').css('display', 'none');
+  } else {
+    $('*.website-table').css('display', 'none');
+    $(this).find('.website-table').css('display', 'block');
+  }
 });
