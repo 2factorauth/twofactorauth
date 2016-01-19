d9dc9256309303145eb831fecae10cca0e8557c0
Carlgo11 <github.private@carlgo11.com>
2016-01-19 20:04:29 +0100

    Add check for more YAML tags

--- verify.rb
+++ verify.rb
@@ -2,6 +2,7 @@
 require 'yaml'
 require 'fastimage'
 $output=0;
+$ignore_twitter=0;
 begin
 
   # Send error message
@@ -15,17 +16,36 @@ begin
 
   # Verify that the tfa factors are booleans
   def check_tfa(website)
-    $tfa_forms = ['email', 'hardware', 'software', 'sms', 'phone']
-    for i in 0..($tfa_forms.length-1)
-      form = website[$tfa_forms[i]]
+    tfa_forms = ['email', 'hardware', 'software', 'sms', 'phone']
+    for i in 0..(tfa_forms.length-1)
+      form = website[tfa_forms[i]]
       if (form != nil)
         if (form != true)
-          error("#{website['name']} contains a \'#{$tfa_forms[i]}\' tag. It\'s value should be \'YES\' or not set.")
+          error("#{website['name']} should not contain a \'#{tfa_forms[i]}\' tag when it\'s value isn\'t \'YES\'.")
         end
         if (website['tfa'] != true)
-          error("#{website['name']} contains a \'#{$tfa_forms[i]}\' tag but tfa is not set to \'YES\'")
+          error("#{website['name']} should not contain a \'#{tfa_forms[i]}\' tag when it doesn\'t support TFA.")
         end
       end
+    end
+  end
+
+  def tags_set(website)
+    tags = ['url', 'img', 'name']
+    for i in 0..(tags.length-1)
+      tag = website[tags[i]]
+      if(tag == nil)
+        error("#{website['name']} doesn\'t contain a \'#{tags[i]}\' tag.")
+      end
+    end
+
+    #if(!$ignore_twitter)
+      twitter = website['twitter']
+      if(twitter != nil)
+        if(website['tfa'])
+          error("#{website['name']} should not contain a \'twitter\' tag as it supports TFA.")
+        end
+     # end
     end
   end
 
@@ -39,8 +59,10 @@ begin
       if (tfa != 'true' && tfa != 'false')
         error("#{website['name']} \'tfa\' tag should be either \'Yes\' or \'No\'. (#{tfa})");
       end
-
       check_tfa(website)
+      tags_set(website)
+
+
       image = "img/#{section[1]['id']}/#{website['img']}"
 
       unless File.exists?(image)
