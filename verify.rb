07f4465f53d40bd09fd7f30dfad78b43d4a3abac
Carlgo11 <github.private@carlgo11.com>
2016-01-19 11:36:11 +0100

    Update Verify script

--- verify.rb
+++ verify.rb
@@ -3,13 +3,30 @@ require 'yaml'
 require 'fastimage'
 $output=0;
 begin
+
+  # Send error message
   def error(msg)
     $output=$output+1;
     if ($output == 1)
       puts "<------------ ERROR ------------>\n"
     end
     puts "#{$output}. #{msg}"
+  end
 
+  # Verify that the tfa factors are booleans
+  def check_tfa(website)
+    $tfa_forms = ['email', 'hardware', 'software', 'sms', 'phone']
+    for i in 0..($tfa_forms.length-1)
+      form = website[$tfa_forms[i]]
+      if (form != nil)
+        if (form != true)
+          error("#{website['name']} contains a \'#{$tfa_forms[i]}\' tag. It\'s value should be \'YES\' or not set.")
+        end
+        if (website['tfa'] != true)
+          error("#{website['name']} contains a \'#{$tfa_forms[i]}\' tag but tfa is not set to \'YES\'")
+        end
+      end
+    end
   end
 
   # Load each section, check for errors such as invalid syntax
@@ -17,13 +34,13 @@ begin
   main = YAML.load_file('_data/sections.yml')
   main.each do |section|
     data = YAML.load_file('_data/' + section[1]['id'] + '.yml')
-
     data['websites'].each do |website|
       tfa = "#{website['tfa']}"
       if (tfa != 'true' && tfa != 'false')
-        error("#{website['name']} 'tfa' should be either 'Yes' or 'No' #{tfa}");
+        error("#{website['name']} \'tfa\' tag should be either \'Yes\' or \'No\'. (#{tfa})");
       end
 
+      check_tfa(website)
       image = "img/#{section[1]['id']}/#{website['img']}"
 
       unless File.exists?(image)
