abfe54b22842bd17bafa52041a5029556dec9b6e
t0p0lin0 <hello@topolino.cash>
2017-10-31 08:53:56 -0400

    Refactor filtering match based on proper URL parameter, not hash

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 // When DOM elements are ready, excluding images
 $(document).ready(function () {
 
+  // Get all URL parameters
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+      
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+    }
+  };
+
+  // Check if URL parameter exists to filter by BCH-only
+  if (getUrlParameter('filter') == 'bch') {
+    $('#show-bcc-only').prop('checked', true);
+  }
+
   // Check if URL references specific category
   if (window.location.hash && window.location.hash.indexOf('#') > -1) {
-    if (window.location.hash == '#bch') {
-      $('#show-bcc-only').prop('checked', true);
-    } else {
-      openCategory(window.location.hash.substring(1));
-    }
+    openCategory(window.location.hash.substring(1));
   }
 
   // Some frilly animations on click of the main Bitcoin Cash logo
