7ad1faef7e5417440e5d632ad06997bfa7491abd
Ken Salomon <kenman345@users.noreply.github.com>
2018-03-15 12:21:32 -0400

    changes based on feedback

--- verify.rb
+++ verify.rb
@@ -50,12 +50,12 @@ def test_img(img, name, imgs)
 end
 # rubocop:enable AbcSize,CyclomaticComplexity
 
-def outputOrdered(websites)
+def output_ordered(websites)
   websites.each do |site|
     puts "  #{site['name']}\n"
   end
 end
-      
+ 
 # Load each section, check for errors such as invalid syntax
 # as well as if an image is missing
 begin
@@ -77,8 +77,8 @@ begin
     # Check section alphabetization
     error("_data/#{section['id']}.yml is not alphabetized by name") \
       if websites != (websites.sort_by { |website| website['name'].downcase })
-        
-	  outputOrdered(websites.sort_by { |website| website['name'].downcase }) \
+
+    output_ordered(websites.sort_by { |website| website['name'].downcase }) \
       if websites != (websites.sort_by { |website| website['name'].downcase })
 
     # Collect list of all images for section
