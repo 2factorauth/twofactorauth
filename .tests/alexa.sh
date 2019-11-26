9417cbb6f387c1ae2153a9077f48a424aa3e9e0c
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-11-26 16:31:49 +0100

    More if statements

--- .tests/alexa.sh
+++ .tests/alexa.sh
@@ -3,7 +3,7 @@
 # Check Alexa rank
 check_rank () {
 	url="$(git --no-pager diff origin/master..HEAD ../_data| grep ^+[[:space:]] | grep url | cut -c11-)"
-	if [ -n "$url" ]; then
+	if [ -z "$url" ]; then
 		alexa_rank="$(ruby alexa.rb ${url})"
 		if [ "$alexa_rank" -gt 200000 ]; then
 			echo "::error::${url} has an Alexa ranking above 200K. (${alexa_rank})"
--- .tests/twitter.sh
+++ .tests/twitter.sh
@@ -3,11 +3,16 @@
 # Check Twitter handle
 check_twitter () {
 	handle="$(git --no-pager diff origin/master..HEAD ../_data | grep ^+[[:space:]] | grep twitter | cut -c15-)"
-	twitter="$(ruby twitter.rb ${handle})"
-	if [ "$twitter" ]; then
-		echo "::error::$twitter"
-		exit 1
-	fi
+  if [ -z "$handle" ]; then
+    echo "No Twitter handles found"
+    exit 0
+  else
+    twitter="$(ruby twitter.rb ${handle})"
+    if [ "$twitter" ]; then
+      echo "::error::$twitter"
+      exit 1
+    fi
+  fi
 }
 
 check_twitter
