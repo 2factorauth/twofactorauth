c0c510736e8680170e3058d63dc2cd36587ed773
Carlgo11 <Carlgo11@users.noreply.github.com>
2019-11-26 18:16:05 +0100

    Move PR checks from GH actions to Travis (#4351)
    * Add travis
    
    * Add travis
    
    * Blank line test
    
    * Fix travis errors
    
    * hopefully remove bundler install
    
    * Fix syntax error
    
    * Fix syntax error
    
    * fix script paths
    
    * More if statements
    
    * Exit alexa.sh with 0
    
    * Add debug comment
    
    * Fix syntax error
    
    * revert if statement
    
    * Shorten travis runtime
    
    * Shorten travis runtime
    
    * Only run travis scripts on PR
    
    * syntax errors

--- .tests/alexa.sh
+++ .tests/alexa.sh
@@ -4,6 +4,7 @@
 check_rank () {
 	url="$(git --no-pager diff origin/master..HEAD ../_data| grep ^+[[:space:]] | grep url | cut -c11-)"
 	if [ -n "$url" ]; then
+    echo "running ruby alexa.rb ${url}"
 		alexa_rank="$(ruby alexa.rb ${url})"
 		if [ "$alexa_rank" -gt 200000 ]; then
 			echo "::error::${url} has an Alexa ranking above 200K. (${alexa_rank})"
@@ -12,6 +13,9 @@ check_rank () {
 			echo "${url} has an Alexa ranking of (${alexa_rank})."
 			exit 0
 		fi
+  else
+    echo "No URLs found"
+    exit 0
 	fi
 }
 
