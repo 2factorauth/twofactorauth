ec7d59f7a55b7cd6824d04da30ff9dc00a5eb56c
Michael Loßin <bugs@michael-lossin.de>
2020-09-09 18:25:13 +0200

    Make substitution case-insensitive to match "-iname" option

--- _deployment/webp.sh
+++ _deployment/webp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # converting PNG images
 find $1 -type f -and -iname "*.png" | while read png_path; do
-    webp_path=${png_path/%.png/.webp}
+    webp_path=${png_path/%.[pP][nN][gG]/.webp}
     if [ ! -f "$webp_path" ]; then
 	cwebp -quiet -preset icon "$png_path" -o "$webp_path"
     fi
