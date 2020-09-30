d85515ccb760d94090cb632311a9262378710a23
Michael Loßin <bugs@michael-lossin.de>
2020-09-30 18:30:04 +0200

    Improved Twitter/Facebook checks
    - ignore whitespace in yaml values
    - no early exit
    - use sed to extract Facebook handle in one go

--- _deployment/tests/facebook.sh
+++ _deployment/tests/facebook.sh
@@ -4,32 +4,32 @@
 check_facebook () {
   handles="$(git diff origin/master...HEAD ../../_data | grep "^+[[:space:]]*facebook:" | cut -c16-)"
 
-  if [ -z "$handles" ]; then
+  if [[ ! "$handles" =~ [^[:space:]] ]]; then
     echo "No Facebook handles found."
     exit 0
   fi
 
+  res=0
   # Loop through all handles
-  echo "${handles}" | while IFS= read -r handle; do
+  for handle in $handles; do
 
-    # Get response from facebook page URL
-    url="$(curl https://www.facebook.com/pg/${handle} -sSI)"
-
-    # Trim out anything that's not a handle
-    fb_handle="$(echo "${url}"| grep location | cut -c 36- | rev | cut -c 3- | rev)"
+    # Get real handle from facebook page location URL
+    fb_handle="$(curl https://www.facebook.com/pg/${handle} -sSI | sed -n '/location/s?.*com/\([^/]*\)/.*?\1?p' )"
 
     # Compare 302 location with the handle
     if [ "$fb_handle" == "$handle" ]; then
       echo -e "\e[32mFacebook page \"${handle}\" is valid.\e[39m"
     elif [ -z "$fb_handle" ]; then
       echo -e "\e[31mFacebook page \"${handle}\" not found.\e[39m"
-      exit 1
+      res=1
     else
       echo -e "\e[31mFacebook handle \"${handle}\" should be \"${fb_handle}\".\e[39m"
-      exit 1
+      res=1
     fi
 
   done
+
+  exit $res
 }
 
 check_facebook
--- _deployment/tests/twitter.sh
+++ _deployment/tests/twitter.sh
@@ -4,26 +4,29 @@
 check_twitter () {
   handles="$(git diff origin/master...HEAD ../../_data | grep "^+[[:space:]]*twitter:" | cut -c15-)"
 
-  if [ -z "$handles" ]; then
+  if [[ ! "$handles" =~ [^[:space:]] ]]; then
     echo "No Twitter handles found."
     exit 0
   fi
 
   gem i twitter --no-post-install-message --no-suggestions --minimal-deps --no-verbose -N -q --silent
 
+  res=0
   # Loop through all Twitter handles
-  echo "${handles}" | while IFS= read -r handle; do
+  for handle in $handles; do
 
     twitter="$(ruby twitter.rb ${handle})"
 
     if [ "$twitter" ]; then
       echo -e "\e[31m$twitter\e[39m"
-      exit 1
+      res=1
     else
       echo -e "\e[32mTwitter handle \"${handle}\" is valid.\e[39m"
     fi
 
   done
+
+  exit $res
 }
 
 check_twitter
