c7d6d7fc36ac47f542ec6a80aec242901620a2c6
Josh Davis <j@joshldavis.com>
2020-11-28 22:44:29 -0800

    Add a cookie consent and basic privacy policy
    - Cookieconsent will look at the user's IP and only show the cookie
      consent if it fails to look it up or they are a country that requires
      explicit opt-in
    - Removes a browser based backbutton which was weird
    - Removes some commented out html

---
+++ js/g.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+  let didSetConsent = false;
+
+  function allowAnalytics() {
+    gtag('consent', 'update', {
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted',
+    });
+  }
+
+  function processConsent() {
+    didSetConsent = true;
+    if (this.hasConsented()) {
+      allowAnalytics();
+    }
+  }
+
+  // Always called after consentcookie finishes, we just need to handle if consentcookie determined
+  // it didn't need to ask the user for consent
+  function onComplete() {
+    if (!didSetConsent) {
+      allowAnalytics();
+    }
+  }
+
+  window.cookieconsent.initialise({
+    container: document.getElementById("consentCookie"),
+    type: 'opt-in',
+    palette: {
+      popup: { background: "#efefef" },
+    },
+    content: {
+      allow: 'Allow',
+      href: '/privacy',
+    },
+    onStatusChange: processConsent,
+    onInitialise: processConsent,
+    location: true,
+  }, onComplete);
+  gtag('js', new Date());
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied',
+    // Delay setting default consent settings until consentcookie has started up
+    'wait_for_update': 500
+  });
+  gtag('config', 'G-SMXGN4C672');
+});
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
