36af9923d50d82b53c16b4a37ea9b1d4c791ed83
Josh Davis <j@joshldavis.com>
2020-11-29 11:13:48 -0800

    Add a more detailed cookie consent message

--- js/g.js
+++ js/g.js
@@ -31,6 +31,7 @@ $(document).ready(function () {
     },
     content: {
       allow: 'Allow',
+      message: 'We use cookies and other tracking technologies to improve your browsing experience on our website to analyze our website traffic and to understand where our visitors are coming from.',
       href: '/privacy',
     },
     onStatusChange: processConsent,
