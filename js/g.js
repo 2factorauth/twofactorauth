d465e0c8a0377b9f589183233b3e496d9fe370da
Josh Davis <jdavis@users.noreply.github.com>
2020-12-11 08:31:00 -0800

    Add GA
    - Adds a basic privacy policy
    - Cookieconsent will look at the user's IP and only show the cookie
      consent if it fails to look it up or they are a country that requires
      explicit opt-in
    - Removes a browser based backbutton which was weird
    - Removes some commented out html
    - Enables DNT for GA
    
    Rename Quizzle to Bankrate
    
    Don't load GA during development
    
    Better description
    
    Remove Valic
    
    Update Cloud66
    
    Update Skrill
    
    Rename NSB to Vy (#5085)
    
    Co-authored-by: Carlgo11 <Carlgo11@users.noreply.github.com>
    
    Removed exception for Colorado School of Mines
    
    Colorado School of Mines previously had an exception of 2FA being "only available for employees," but 2FA is now available to all current Mines users.
    
    Remove regional stuff
    
    - These pages are getting very little traffic
    - The info could be useful but not presented the best
    
    Removing it for now and it'll be revisited.
    
    Update finance.yml
    
    Updated Northwestern Mutual to use software token, sms, and phone, no public facing documentation available
    
    Update _data/finance.yml
    
    updated to use suggested document
    
    Co-authored-by: Josh Davis <jdavis@users.noreply.github.com>
    
    Remove HelloWallet
    
    Breakout some of the 2FA details of major US banks into landing pages
    
    The site has a lot of great information but it isn't very accessible via
    google search because of the site's formatting
    
    As a small, hacky-ish, test, try breaking out the banking pages into
    individual landing pages and see if this ends up being more user
    friendly in terms of google search and site experience
    
    Hide build version, don't install webp in the bg
    
    Remove desktop only table class....
    
    This looks bad but tables need improved. Not sure why on the main page
    two tables (one for mobile and one for desktop) are shown and then
    hidden based on the viewport size. A proper responsive table should be
    created.
    
    This is just a stopgap until that is done.
    
    LastPass does not support U2F.
    
    Add teespring
    
    Updated LCN

---
+++ js/g.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  if (window.location.hostname === 'localhost') {
+    return;
+  }
+
+  let didSetConsent = false;
+
+  function allowAnalytics() {
+    gtag('consent', 'update', {
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted',
+    });
+  }
+
+  function processConsent() {
+    didSetConsent = true;
+    if (this.hasConsented()) {
+      allowAnalytics();
+    }
+  }
+
+  // Always called after consentcookie finishes, we just need to handle if consentcookie determined
+  // it didn't need to ask the user for consent
+  function onComplete() {
+    if (!didSetConsent) {
+      allowAnalytics();
+    }
+  }
+
+  window.cookieconsent.initialise({
+    container: document.getElementById("consentCookie"),
+    type: 'opt-in',
+    palette: {
+      popup: { background: "#efefef" },
+    },
+    content: {
+      allow: 'Allow',
+      message: 'We use cookies and other tracking technologies to improve your browsing experience on our website to analyze our website traffic and to understand where our visitors are coming from.',
+      href: '/privacy',
+    },
+    onStatusChange: processConsent,
+    onInitialise: processConsent,
+    location: true,
+  }, onComplete);
+  gtag('js', new Date());
+  gtag('consent', 'default', {
+    'ad_storage': 'denied',
+    'analytics_storage': 'denied',
+    // Delay setting default consent settings until consentcookie has started up
+    'wait_for_update': 500
+  });
+  gtag('require', 'dnt');
+  gtag('config', 'G-SMXGN4C672');
+});
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
