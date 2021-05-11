2d33a65bf54654a823d2cfcf353213a5d66dbf74
Carlgo11 <carlgo11@users.noreply.github.com>
2021-05-12 00:39:53 +0200

    Update regions.rb & clean up minify-js.sh

--- scripts/minify-js.sh
+++ scripts/minify-js.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for file in _site/js/*.js; do
-  ./node_modules/.bin/babel-minify "$file" -o "$file" --simplifyComparisons --simplify --mangle
+  babel-minify "$file" -o "$file" --simplifyComparisons --simplify --mangle
 done
