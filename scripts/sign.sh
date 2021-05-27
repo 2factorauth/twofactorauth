784994ef744980ef61c187dd9200a2b2890e4da5
Carlgo11 <carlgo11@users.noreply.github.com>
2021-05-27 02:56:58 +0200

    Run sign.sh from _site

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -2,5 +2,7 @@
 
 for file in api/v*/*.json; do
 echo "$file.sig"
-echo $1 | gpg --yes --passphrase --local-user $2 --output "$file.sig" --sign "$file"
+echo "$1" | gpg --yes --passphrase --local-user "$2" --output "$file.sig" --sign "$file" 2>/dev/null
+gpg --verify "$file.sig" 2>/dev/null
+[[ $? -eq 0 ]] || { exit 1; }
 done
