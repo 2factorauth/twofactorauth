45742f47bfaa1711369d48af93538b6bf41c80cf
Arun Pattni <me@arunpattni.com>
2021-06-02 20:09:28 +0100

    more useful output and validation

--- tests/validate-images.rb
+++ tests/validate-images.rb
@@ -11,11 +11,11 @@ begin
     website = JSON.parse(File.read(file)).values[0]
     path = './img/'
     if website['img'].nil?
-      path += website['domain'][0,1] + '/' + website['domain'] + '.svg'
+      path += website['domain'][0,1].downcase + '/' + website['domain'] + '.svg'
     else
       path += website['img'][0,1] + '/' + website['img']
     end
-    raise('Image does not exist') unless File.exist?(path)
+    raise("Image does not exist for #{website['domain']}") unless File.exist?(path)
   end
 rescue StandardError => e
   puts ":: error file=#{f}:: #{e.message}"
