13d3bedb01120db13bddb722e03df6a6e6117fb9
Arun Pattni <me@arunpattni.com>
2021-06-02 19:57:55 +0100

    Add Image Existence test

---
+++ tests/validate-images.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'json'
+status = 0
+
+begin
+  f = nil
+  Dir.glob('entries/*/*.json') do |file|
+    f = file
+    website = JSON.parse(File.read(file)).values[0]
+    path = './img/'
+    if website['img'].nil?
+      path += website['domain'][0,1] + '/' + website['domain'] + '.svg'
+    else
+      path += website['img'][0,1] + '/' + website['img']
+    end
+    raise('Image does not exist') unless File.exist?(path)
+  end
+rescue StandardError => e
+  puts ":: error file=#{f}:: #{e.message}"
+  status = 1
+end
+
+exit(status)
