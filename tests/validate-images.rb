75bcc9e9a2575a10d925200ecead3de6b0bacdcf
Carlgo11 <carlgo11@users.noreply.github.com>
2021-06-03 15:03:50 +0200

    Ran validate-images.rb through rubocop

--- tests/validate-images.rb
+++ tests/validate-images.rb
@@ -4,52 +4,40 @@
 require 'json'
 status = 0
 
-accepted_extensions = [".png", ".svg"]
+accepted_extensions = %w[.png .svg]
 
 seen_sites = []
 
-begin
-  f = nil
-  Dir.glob('entries/*/*.json') do |file|
+Dir.glob('entries/*/*.json') do |file|
+  website = JSON.parse(File.read(file)).values[0]
+  path = './img/'
 
-    f = file
-    website = JSON.parse(File.read(file)).values[0]
-    path = './img/'
+  path += if website['img'].nil?
+            "#{website['domain'][0, 1].downcase}/#{website['domain']}.svg"
+          else
+            "#{website['img'][0, 1]}/#{website['img']}"
+          end
 
-    if website['img'].nil?
-      path += website['domain'][0,1].downcase + '/' + website['domain'] + '.svg'
-    else
-      path += website['img'][0,1] + '/' + website['img']
-    end
-
-    unless File.exist?(path)
-      puts "::error file=#{file}:: Image does not exist for #{website['domain']} - #{path} cannot be found"
-      status = 1
-    end
-
-    seen_sites.push(path)
-
+  unless File.exist?(path)
+    puts "::error file=#{file}:: Image does not exist for #{website['domain']} - #{path} cannot be found"
+    status = 1
   end
 
-  Dir.glob('img/*/*') do |file|
-    f = file
+  seen_sites.push(path)
+end
 
-    if !f.include? '/icons/'
+Dir.glob('img/*/*') do |file|
+  next if file.include? '/icons/'
 
-      unless seen_sites.include? './' + f
-        puts "::error file=#{f}:: Unused image at #{f}"
-        status = 1
-      end
-
-      unless accepted_extensions.include? File.extname(f)
-        puts "::error file=#{f}:: Invalid file extension for #{f}. Only #{accepted_extensions} are allowed"
-        status = 1
-      end
-
-    end
-    
+  unless seen_sites.include? "./#{file}"
+    puts "::error file=#{file}:: Unused image at #{file}"
+    status = 1
   end
 
+  unless accepted_extensions.include? File.extname(file)
+    puts "::error file=#{file}:: Invalid file extension for #{file}. Only #{accepted_extensions} are allowed"
+    status = 1
+  end
 end
 
 exit(status)
