6e0ddbcd718d82924781ed784791c15db143243c
Arun Pattni <me@arunpattni.com>
2021-06-20 20:09:30 +0100

    Fix spelling error

--- tests/validate-json.rb
+++ tests/validate-json.rb
@@ -33,10 +33,10 @@ Dir.glob('entries/*/*.json') do |file|
   
   file_name = file.split('/')[2]
   expected_file_name = document.values[0]['domain'] + '.json'
-  
+
   unless file_name.eql? expected_file_name
     puts "::error file=#{file}:: File name should be the same as the domain name.
-    Recieved: #{file_name}. Expected: #{expected_file_name}"
+    Received: #{file_name}. Expected: #{expected_file_name}"
     status = 1
   end
 end
