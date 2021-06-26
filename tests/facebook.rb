71ebba0a2c686884761ce1aa01480fc9467ba563
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-06-26 17:04:22 +0200

    Update tests/facebook.rb
    Co-authored-by: Michael Loßin <bugs@michael-lossin.de>

--- tests/facebook.rb
+++ tests/facebook.rb
@@ -6,7 +6,7 @@ require 'uri'
 
 status = 0
 diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"facebook"[^"]*"\\(.*\\)".*/\\1/p'`
-diff.split(' ').each do |page|
+diff.split("\n").each do |page|
   url = URI("https://www.facebook.com/pg/#{page}")
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
