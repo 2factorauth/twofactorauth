dec35473f03f1d9b5b8178478eb9f70f5575caf1
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-07-08 21:00:52 +0200

    Add URL/Domain test (#5941)
    * Add URL/Domain test
    
    * Apply suggestions from code review
    
    Co-authored-by: Arun Pattni <me@arunpattni.com>
    
    Co-authored-by: Arun Pattni <me@arunpattni.com>

---
+++ tests/url.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'English'
+domain_diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"domain"[^"]*"\\(.*\\)".*/\\1/p'`
+url_diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"url"[^"]*"\\(.*\\)".*/\\1/p'`
+status = 0
+
+domain_diff.split("\n").each do |site|
+  next if url_diff.include?(site)
+
+  puts `curl -sSI https://#{site}/`
+  status = 1 unless $CHILD_STATUS.success?
+end
+url_diff.split("\n").each do |site|
+  puts `curl -sSI #{site}`
+  status = 1 unless $CHILD_STATUS.success?
+end
+
+exit(status)
