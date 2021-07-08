5437ae99dc32cbeb483b1cd9c978ab8c82390366
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-07-08 19:41:29 +0200

    Apply suggestions from code review
    Co-authored-by: Arun Pattni <me@arunpattni.com>

--- tests/url.rb
+++ tests/url.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'English'
 domain_diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"domain"[^"]*"\\(.*\\)".*/\\1/p'`
 url_diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"url"[^"]*"\\(.*\\)".*/\\1/p'`
 status = 0
@@ -9,11 +10,11 @@ domain_diff.split("\n").each do |site|
   next if url_diff.include?(site)
 
   puts `curl -sSI https://#{site}/`
-  status = 1 unless $CHILD_STATUS
+  status = 1 unless $CHILD_STATUS.success?
 end
 url_diff.split("\n").each do |site|
   puts `curl -sSI #{site}`
-  status = 1 unless $CHILD_STATUS
+  status = 1 unless $CHILD_STATUS.success?
 end
 
 exit(status)
