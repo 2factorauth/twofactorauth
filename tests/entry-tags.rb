9442048b079cecf3921103d749ca00f6c8a0a460
Arun Pattni <me@arunpattni.com>
2021-07-19 14:25:46 +0100

    Improve Entry Tags Test

--- tests/entry-tags.rb
+++ tests/entry-tags.rb
@@ -3,20 +3,49 @@
 
 require 'json'
 status = 0
-begin
-  f = nil
-  Dir.glob('entries/*/*.json') do |file|
-    f = file
-    website = JSON.parse(File.read(file)).values[0]
-    if website['tfa'].nil?
-      raise('"notes" requires "tfa" to be set.') unless website['notes'].nil?
-    else
-      raise('Contact information shouldn\'t be present when "tfa" is set.') unless website['contact'].nil?
+
+Dir.glob('entries/*/*.json') do |file|
+  website = JSON.parse(File.read(file)).values[0]
+
+  if website['tfa'].nil?
+    unless website['notes'].nil?
+      puts "::error file=#{file}:: 'notes' requires 'tfa' to be set"
+      status = 1
+    end
+    unless website['documentation'].nil?
+      puts "::error file=#{file}:: 'documentation' requires 'tfa' to be set"
+      status = 1
+    end
+    unless website['recovery'].nil?
+      puts "::error file=#{file}:: 'recovery' requires 'tfa' to be set"
+      status = 1
+    end
+    unless website['custom-software'].nil? && website['custom-hardware'].nil?
+      puts "::error file=#{file}:: 'custom-property' requires 'tfa' to be set"
+      status = 1
+    end
+  else
+    unless website['contact'].nil?
+      puts "::error file=#{file}:: Contact information shouldn't be present when 'tfa' is set"
+      status = 1
+    end
+
+    if website['custom-software']
+      unless website['tfa'].include? 'custom-software'
+        puts "::error file=#{file}:: 'tfa' must include 'custom-software' when 'custom-software' is set"
+      end
+    end
+    if website['custom-hardware']
+      unless website['tfa'].include? 'custom-hardware'
+        puts "::error file=#{file}:: 'tfa' must include 'custom-hardware' when 'custom-hardware' is set"
+      end
     end
   end
-rescue StandardError => e
-  puts ":: error file=#{f}:: #{e.message}"
-  status = 1
+
+  if website['tfa'].nil? && website['contact'].nil?
+    puts "::error file=#{file}:: One of 'contact' or 'tfa' must be set depending on whether the site supports 2FA"
+    status = 1
+  end
 end
 
 exit(status)
