6841289ac2f5dbd7c25e3cae874f08076b731576
Arun Pattni <me@arunpattni.com>
2021-08-01 18:14:17 +0100

    Fix sorting order

--- scripts/APIv3.rb
+++ scripts/APIv3.rb
@@ -11,5 +11,5 @@ all.sort.to_h.each do |k, v|
   v['tfa']&.each { |method| (tfa[method].nil? ? tfa[method] = { k => v } : tfa[method][k] = v) }
   v['regions']&.each { |region| regions[region] = 1 + regions[region].to_i }
 end
-{ 'all' => all }.merge(tfa).each { |k, v| File.open("api/v3/#{k}.json", 'w') { |file| file.write v.sort.to_json } }
+{ 'all' => all }.merge(tfa).each { |k, v| File.open("api/v3/#{k}.json", 'w') { |file| file.write v.sort_by{ |k,v| k.downcase }.to_json } }
 File.open('api/v3/regions.json', 'w') { |file| file.write regions.sort_by(&:last).reverse.to_h.to_json }
