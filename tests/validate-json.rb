78593435abea43071fead183c4a7dfbb95f5cf92
Arun Pattni <20560218+ApCoder123@users.noreply.github.com>
2021-08-24 23:20:19 +0100

    Check for duplicate site names (#6066)
    * Check for duplicate site names
    
    * fix error
    
    * Move duplicate name checking
    
    * Skip tests on schema error
    
    * exit -> next
    
    Co-authored-by: Michael Loßin <bugs@michael-lossin.de>
    
    * Fix rubocop warnings and errors
    
    Co-authored-by: Carlgo11 <Carlgo11@users.noreply.github.com>
    Co-authored-by: Michael Loßin <bugs@michael-lossin.de>

--- tests/validate-json.rb
+++ tests/validate-json.rb
@@ -3,27 +3,32 @@
 
 require 'json_schemer'
 
-status = 0
+@status = 0
+seen_names = []
 
 schema = JSONSchemer.schema(File.read('tests/schema.json'))
-categories = JSON.parse(File.read('_data/categories.json')).map { |cat| cat['name'] }
+categories = JSON.parse(File.read('_data/categories.json')).map { |category| category['name'] }
 
+def error(file, msg)
+  @status = 1
+  puts "::error file=#{file}:: #{msg}"
+end
+
+# rubocop:disable Metrics/BlockLength
 Dir.glob('entries/*/*.json') do |file|
   begin
     JSON.parse(File.read(file))
   rescue JSON::ParserError => e
-    puts "::error file=#{file}:: Invalid JSON in #{file}\n#{e.full_message}"
-    status = 1
+    error(file, "Invalid JSON in #{file}\n#{e.full_message}")
     next
   end
 
   document = JSON.parse(File.read(file))
 
-  unless schema.valid?(document)
-
+  unless schema.valid? document
     schema.validate(document).each do |v|
       puts ''
-      puts "::error file=#{file}::#{v['type'].capitalize} error in in #{file}"
+      puts "::error file=#{file}:: '#{v['type'].capitalize}' error in in #{file}"
       puts "- tag: #{v['data_pointer'].split('/')[2]}" if v['data_pointer'].split('/').length >= 3
       puts "  data: #{v['data']}" if v['details'].nil?
       puts "  data: #{v['details']}" unless v['details'].nil?
@@ -33,33 +38,40 @@ Dir.glob('entries/*/*.json') do |file|
       puts "  expected: only one of 'tfa' or 'contact'" if v['type'].eql?('oneOf')
       puts "  expected: 'tfa' to contain '#{v['schema']['contains']['const']}'" if v['type'].eql?('contains')
     end
-    status = 1
+    @status = 1
+    next
   end
 
   domain = document.values[0]['domain']
   url = document.values[0]['url']
   default_url = "https://#{domain}"
-  if !url.nil? && ( url.eql?(default_url) || url.eql?(default_url+"/") )
-    puts "::error file=#{file}:: Defining the url property for #{domain} is not necessary - '#{default_url}' is the default value"
-    status = 1
+  if !url.nil? && (url.eql?(default_url) || url.eql?("#{default_url}/"))
+    error(file, "Defining the url property for #{domain} is not necessary - '#{default_url}' is the default value")
   end
-  
+
   keywords = document.values[0]['keywords']
   keywords.each do |kw|
     unless categories.include? kw
-      puts "::error file=#{file}:: Invalid keyword: '#{kw}'. See _data/categories.json for a list of valid keywords"
-      status = 1
+      error(file, "Invalid keyword: '#{kw}'. See _data/categories.json for a list of valid keywords")
     end
   end
-  
+
   file_name = file.split('/')[2]
-  expected_file_name = document.values[0]['domain'] + '.json'
+  expected_file_name = "#{document.values[0]['domain']}.json"
 
   unless file_name.eql? expected_file_name
-    puts "::error file=#{file}::File name should be the same as the domain name.
-    Received: #{file_name}. Expected: #{expected_file_name}"
-    status = 1
+    error(file,
+          "File name should be the same as the domain name. Received: #{file_name}. Expected: #{expected_file_name}")
+  end
+
+  name = document.keys[0]
+  if seen_names.include? name
+    error(file, "An entry with the name '#{name}' already exists. Duplicate site names are not allowed.
+    If this entry is not the same site, please rename '#{name}'.")
+  else
+    seen_names.push(name)
   end
 end
+# rubocop:enable Metrics/BlockLength
 
-exit(status)
+exit(@status)
