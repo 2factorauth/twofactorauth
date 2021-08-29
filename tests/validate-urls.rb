592fd296c4c6ff486c670a5a1fbc7ee8d94f348e
Carlgo11 <carlgo11@users.noreply.github.com>
2021-08-29 05:10:53 +0200

    Rework cURL test

---
+++ tests/validate-urls.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'English'
+require 'json'
+diff = `git diff --name-only --diff-filter=AM origin/master...HEAD entries/ `.split("\n")
+
+def curl(url, path)
+  puts `curl --fail -sSI #{url}`
+  puts "::error file=#{path}:: Unable to reach #{url}" unless $CHILD_STATUS.success?
+end
+
+diff.each do |path|
+  entry = JSON.parse(File.read(path)).values[0]
+
+  if entry.key? 'url'
+    curl(entry['url'], path)
+  else
+    curl("https://#{entry['domain']}/", path)
+    entry['additional-domains'].each { |domain| curl("https://#{domain}/", path) }
+  end
+
+  curl(entry['documentation'], path) if entry.key? !entry['documentation'].start_with?('/notes/')
+  curl(entry['recovery'], path) if entry.key? 'recovery'
+
+end
