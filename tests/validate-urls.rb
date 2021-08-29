e53cc14ecb58bbf318596e92cd0ec3249718b072
Carlgo11 <carlgo11@users.noreply.github.com>
2021-08-29 20:57:03 +0200

    Clean up & add comments

--- tests/validate-urls.rb
+++ tests/validate-urls.rb
@@ -3,23 +3,26 @@
 
 require 'English'
 require 'json'
+
+# Fetch created/modified files in entries/**
 diff = `git diff --name-only --diff-filter=AM origin/master...HEAD entries/`.split("\n")
 
-def curl(url, path)
+def curl(url)
   puts `curl --fail -sSI #{url}`
-  puts "::error file=#{path}:: Unable to reach #{url}" unless $CHILD_STATUS.success?
+  # Break build if above cURL exited with non-zero value
+  puts "::error file=#{@path}:: Unable to reach #{url}" unless $CHILD_STATUS.success?
 end
 
 diff.each do |path|
-  entry = JSON.parse(File.read(path)).values[0]
+  # Make path global for curl()
+  @path = path
+  entry = JSON.parse(File.read(@path)).values[0]
 
-  if entry.key? 'url'
-    curl(entry['url'], path)
-  else
-    curl("https://#{entry['domain']}/", path)
-    entry['additional-domains'].each { |domain| curl("https://#{domain}/", path) }
-  end
+  # Process the url,domain & additional-domains
+  curl((entry.key?('url') ? entry['url'] : "https://#{entry['domain']}/"))
+  entry['additional-domains'].each { |domain| curl("https://#{domain}/") }
 
-  curl(entry['documentation'], path) if entry.key? !entry['documentation'].start_with?('/notes/')
-  curl(entry['recovery'], path) if entry.key? 'recovery'
+  # Process documentation and recovery URLs
+  curl(entry['documentation']) if entry.key? !entry['documentation'].start_with?('/notes/')
+  curl(entry['recovery']) if entry.key? 'recovery'
 end
