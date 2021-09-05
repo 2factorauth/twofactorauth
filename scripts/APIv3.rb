ccdaeef2e89ac4a25d9bce41e29db239e204b10a
Arun Pattni <me@arunpattni.com>
2021-09-05 15:12:41 +0100

    Modify APIv3.rb to produce tfa.json

--- scripts/APIv3.rb
+++ scripts/APIv3.rb
@@ -5,11 +5,14 @@ require 'json'
 
 all = {}
 tfa = {}
+all_2fa = {}
 regions = {}
 Dir.glob('entries/*/*.json') { |file| all[JSON.parse(File.read(file)).keys[0]] = JSON.parse(File.read(file)).values[0] }
 all.sort.to_h.each do |k, v|
   v['tfa']&.each { |method| (tfa[method].nil? ? tfa[method] = { k => v } : tfa[method][k] = v) }
   v['regions']&.each { |region| regions[region] = 1 + regions[region].to_i }
+  all_2fa[k] = v if v['tfa']
 end
 { 'all' => all }.merge(tfa).each { |k, v| File.open("api/v3/#{k}.json", 'w') { |file| file.write v.sort_by{ |k,v| k.downcase }.to_json } }
 File.open('api/v3/regions.json', 'w') { |file| file.write regions.sort_by(&:last).reverse.to_h.to_json }
+File.open('api/v3/tfa.json', 'w') { |file| file.write all_2fa.sort_by{ |k,v| k.downcase }.to_json }
