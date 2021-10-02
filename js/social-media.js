2a9c2c4e53593d1af13bdb7d698dc7850f4a6725
Arun Pattni <20560218+ApCoder123@users.noreply.github.com>
2021-10-02 20:12:19 +0100

    Append URL to tweet message (#6124)
    * Append URL to tweet message
    
    * Add URL to site config
    
    * site.github.url
    
    * Revert "site.github.url"
    
    This reverts commit 1aba2e2272a2998a44fb8788466ffb793f561696.
    
    * Use intent uri rather than share
    
    https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/guides/web-intent
    
    Co-authored-by: Michael Loßin <github@michael-lossin.de>

--- js/social-media.js
+++ js/social-media.js
@@ -11,10 +11,10 @@ $('.email-button').click(function () {
   langs.set("{{ lang[0] }}", "{{ lang[1].email_subject }}");
   {% endif %}
 {% endfor %}
-let lang = $(this).data('lang')
+  let lang = $(this).data('lang');
 
-  if (!langs.has(lang)) lang = "en"
-  window.open('mailto:' + $(this).data('email') + '?subject=' + langs.get(lang))
+  if (!langs.has(lang)) lang = "en";
+  window.open('mailto:' + $(this).data('email') + '?subject=' + langs.get(lang));
 })
 
 $('.twitter-button').click(function () {
@@ -24,18 +24,20 @@ $('.twitter-button').click(function () {
   {% if lang[1].tweets %}
     tweets = [];
     {% for tweet in lang[1].tweets %}
-      tweets.push("{{ tweet |cgi_escape }}")
+      tweets.push("{{ tweet |cgi_escape }}");
     {% endfor %}
     langs.set("{{ lang[0] }}", tweets);
   {% endif %}
 {% endfor %}
 
-let lang = $(this).data('lang')
-  const handle = $(this).data('twitter')
+  let lang = $(this).data('lang');
+  const handle = $(this).data('twitter');
 
-  if (!langs.has(lang) || lang == null) lang = "en"
+  if (!langs.has(lang) || lang == null) lang = "en";
   const index = Math.floor(Math.random() * langs.get(lang).length);
-  const text = langs.get(lang)[index].replace('TWITTERHANDLE', handle)
+  const text = langs.get(lang)[index].replace('TWITTERHANDLE', handle);
+  const url = "{{ site.url | cgi_escape }}";
+  const tfa_handle = "{{ site.twitter | cgi_escape }}";
 
-  window.open('https://twitter.com/share?hashtags=SupportTwoFactorAuth&text=' + text, '_blank');
+  window.open(`https://twitter.com/intent/tweet?text=${text}&url=${url}&hashtags=SupportTwoFactorAuth&related=${tfa_handle}`, '_blank');
 })
