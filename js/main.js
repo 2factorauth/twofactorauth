7dfa702777d5f733e847c379d63a0539e8d683d2
Jonathan Horak <899369+jareha@users.noreply.github.com>
2022-01-24 11:30:11 -0600

    Move codeblock inside `resizeObserver`

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
-  // Execute when window resized
-  $(window).resize(function() {
-    const width = $(window).innerWidth(); // New width
-
-    if (width >= 992) {
-      // Focus and select search input
-      $('#innerSearchBox').focus().select();
-    }
-  });
-  
   // Show popup notice
   $('.exception').popup({ position: 'right center', hoverable: true, title: 'Exceptions & Restrictions' });
 
@@ -51,10 +41,15 @@ function showCategory(category) {
 
 let resizeObserver = new ResizeObserver(() => {
   // Fix the footer to bottom of viewport if body is less than viewport
-  if ($('body').height() < $(window).height()){
+  if ($('body').height() < $(window).height()) {
     $('.footer').css({position: 'absolute'});
-  } else{
+  } else {
     $('.footer').css({position: 'static'});
+  }
+
+  // Focus and select search input on desktop
+  if ($(window).innerWidth() >= 992) {
+    $('#innerSearchBox').focus().select();
   }
 });
 
