9819e0eee33da1195235ca1214369e62e7901a37
Carlgo11 <Carlgo11@users.noreply.github.com>
2022-02-09 15:03:48 +0100

    New theme proposal (#6484)

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
 $(document).ready(function () {
-  // Show popup notice
-  $('.exception').popup({ position: 'right center', hoverable: true, title: 'Exceptions & Restrictions' });
+  // Make category buttons square
+  $('.box').height($('.box').width());
+
+  // Show region notice
+  if (window.localStorage.getItem('region-notice') !== 'hidden') $('#region-notice').collapse('show');
 
   // Register service worker
   if ('serviceWorker' in navigator) navigator.serviceWorker.register('/service-worker.js');
 
   // Show category of query
   const query = window.location.hash;
-  if (query && query.indexOf('#') > -1) {
-    // Remove all tables before showing the correct one
-    $('.collapse').collapse('hide');
-    showCategory(query.substring(1));
-  }
+  if (query && query.indexOf('#') > -1) showCategory(query.substring(1));
 });
 
+$(window).on('hashchange', function () {
+  const query = window.location.hash;
+  if (query && query.indexOf('#') > -1) showCategory(query.substring(1));
+});
+
+$('.exception').popup({position: 'right center', hoverable: true, title: 'Exceptions & Restrictions'});
+
 // On category click
-$('.cat').click(function () {
-  let query = window.location.hash;
+$('.category-btn').click(function () {
+  let query = window.location.hash.substring(1);
 
   // Collapse all other tables
-  $('.collapse').collapse('hide');
-  $('.cat').removeClass('active');
+  $('.category-table.collapse').collapse('hide');
+  $('.category-btn').removeClass('active');
 
   // Check if category tables are displayed
-  if (!$(`#${query.substring(1)}-table`).hasClass('collapsing') && !$(`#${query.substring(1)}-mobile-table`).hasClass('collapsing') || query.substring(1) !== this.id) {
+  if (!$(`#${query}-table`).hasClass('collapsing') && !$(`#${query}-mobile-table`).hasClass('collapsing') || query !== this.id) {
     window.location.hash = this.id;
     showCategory(this.id);
   } else {
@@ -32,10 +38,17 @@ $('.cat').click(function () {
   }
 });
 
+$('#region-notice-close-btn').click(function () {
+  $('#region-notice').collapse('hide');
+  window.localStorage.setItem('region-notice', 'hidden');
+});
+
 // Show desktop and mobile tables
 function showCategory(category) {
+  $('.category-table.collapse').collapse('hide');
   $(`#${category}-table`).collapse("show");
   $(`#${category}-mobile-table`).collapse("show");
+  $('.category-btn').removeClass('active');
   $(`[id=${category}]`).addClass('active');
 }
 
@@ -47,10 +60,8 @@ let resizeObserver = new ResizeObserver(() => {
     $('.footer').css({position: 'static'});
   }
 
-  // Focus and select search input on desktop
-  if ($(window).innerWidth() >= 992) {
-    $('#innerSearchBox').focus().select();
-  }
+  // Resize square divs
+  $('.box').height($('.box').width());
 });
 
 resizeObserver.observe($('body')[0]);
