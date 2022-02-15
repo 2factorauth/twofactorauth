cca837f32fa52a6c6d291e861a0dc4fc81ebec25
Michael Loßin <github@michael-lossin.de>
2022-02-15 20:40:23 +0100

    Update tests/similarweb.rb
    Co-authored-by: Carlgo11 <Carlgo11@users.noreply.github.com>

--- tests/similarweb.rb
+++ tests/similarweb.rb
@@ -2,55 +2,60 @@
 # frozen_string_literal: true
 
 require 'addressable'
-require 'uri'
-require 'net/http'
-require 'json'
 require 'fileutils'
+require 'json'
+require 'net/http'
+require 'uri'
 
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/MethodLength
-def fetch_from_api(site)
-  apikey = ENV['SIMILARWEB_API_KEY']
-  url = URI("https://api.similarweb.com/v1/similar-rank/#{site}/rank?api_key=#{apikey}")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  headers = { 'Accept' => 'application/json' }
-  response = http.request(Net::HTTP::Get.new(url, headers))
-  raise("(#{response.code}) Request failed.") unless response.code == '200'
+# Cache handling
+module Cache
+  @cache_dir = '/tmp/similarweb/'
 
-  rank = JSON.parse(response.body)['similar_rank']['rank']
-  store_cache(site, rank)
-  raise("#{site} doesn't have a Similarweb ranking") if rank.nil?
-  raise("#{site} is ranked above the maximum rank of 200K") if rank.to_i > 200_000
-end
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/MethodLength
+  def self.fetch(site)
+    path = "#{@cache_dir}#{site}"
+    File.read(path) if File.exist?(path)
+  end
 
-def fetch_from_cache(site)
-  path = "/tmp/similarweb/#{site}"
-  return false unless File.exist?(path)
-
-  rank = File.read(path)
-  raise("#{site} doesn't have a Similarweb ranking") if rank.eql?('')
-  raise("#{site} is ranked above the maximum rank of 200K") if rank.to_i > 200_000
-
-  rank
+  def self.store(site, rank)
+    FileUtils.mkdir_p(@cache_dir)
+    File.open("#{@cache_dir}#{site}", 'w') { |file| file.write rank }
+  end
 end
 
-def store_cache(site, rank)
-  FileUtils.mkdir_p('/tmp/similarweb/')
-  File.open("/tmp/similarweb/#{site}", 'w') { |file| file.write rank }
+# Similarweb API handling
+module Similarweb
+  def self.connect(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.request(Net::HTTP::Get.new(url, { 'Accept' => 'application/json' }))
+  end
+
+  def self.fetch(site)
+    apikey = ENV['SIMILARWEB_API_KEY']
+    url = URI("https://api.similarweb.com/v1/similar-rank/#{site}/rank?api_key=#{apikey}")
+    response = connect(url)
+    raise("#{site} doesn't have a Similarweb ranking") if response.code.eql? '404'
+    raise("(#{response.code}) Request failed.") unless response.code.eql? '200'
+
+    rank = JSON.parse(response.body)['similar_rank']['rank']
+    Cache.store(site, rank)
+    rank
+  end
 end
+
+raise('Similarweb API key not set') if ENV['SIMILARWEB_API_KEY'].nil?
 
 status = 0
 # Fetch changes
-diff = `git diff origin/master...HEAD entries/ | sed -n 's/^+.*"domain"[^"]*"\\(.*\\)".*/\\1/p'`
+diff = `git diff entries/ | sed -n 's/^+.*"domain"[^"]*"\\(.*\\)".*/\\1/p'`
 # Strip and loop through diff
 diff.split("\n").each do |site|
   domain = Addressable::URI.parse("https://#{site}").domain
-  fetch_from_api(domain) unless fetch_from_cache(domain)
+  rank = Cache.fetch(domain) || Similarweb.fetch(domain)
+  raise("#{site} is ranked above the maximum rank of 200K") if rank.to_i > 200_000
 rescue StandardError => e
   puts "\e[31m#{e.message}\e[39m"
-  # status = 1
+  status = 1
 end
-exit(status)
+exit(status) if Time.now.to_i > 1_651_363_200
+
