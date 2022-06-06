e7f36f205adace2295722428c788bec9a76e9a02
Carlgo11 <carlgo11@users.noreply.github.com>
2022-06-06 20:03:13 +0200

    Revert int page changes

--- scripts/regions.rb
+++ scripts/regions.rb
@@ -8,22 +8,19 @@ require 'parallel'
 
 data_dir = './_data'
 categories = JSON.parse(File.read("#{data_dir}/categories.json"))
-websites = JSON.parse(File.read("#{data_dir}/all.json"))
-regions_array = YAML.load_file("#{data_dir}/regions.yml")
-regions = regions_array.map { |r| [r['id'], r] }.to_h
+regions = YAML.load_file("#{data_dir}/regions.yml").map { |r| [r['id'], r] }.to_h
 regions['int'] = { 'name' => 'Global' }
-tmp_dir = '/tmp/2fa'
-git_dir = Dir.glob('.git')
-FileUtils.cp_r(git_dir, "#{tmp_dir}/") unless File.exist?("#{tmp_dir}/.git")
+tmp_dir = `mktemp -d`.strip
+
+FileUtils.cp_r(Dir.glob('.git'), "#{tmp_dir}/") unless File.exist?("#{tmp_dir}/.git")
 
 # Region loop
 # rubocop:disable Metrics/BlockLength
-Parallel.each(-> { regions.pop || Parallel::Stop }) do |region|
-  dest_dir = "#{tmp_dir}/#{region['id']}"
+Parallel.map(regions.keys) do |key|
+  region = regions[key]
+  dest_dir = "#{tmp_dir}/#{key}"
   Dir.mkdir(dest_dir) unless File.exist?(dest_dir)
-  files = %w[index.html _includes _layouts]
-  FileUtils.cp_r(files, dest_dir)
-
+  FileUtils.cp_r(%w[index.html _includes _layouts], dest_dir)
   File.open("#{dest_dir}/_config_region.yml", 'w') do |file|
     file.write("title: 2FA Directory (#{region['name']})") unless key.eql?('int')
   end
@@ -32,13 +29,11 @@ Parallel.each(-> { regions.pop || Parallel::Stop }) do |region|
   used_categories = []
 
   # Website loop
-  websites.each do |name, website|
-    unless website['regions'].nil?
-      site_excluded_regions = website['regions'].select { |r| r.start_with?('-') }.map! { |rc| rc.tr('-', '') }
-      website['regions'] = website['regions'].reject { |r| r.start_with?('-') }
-      next unless website['regions'].include?(key) || key.eql?('int')
-      next if website['regions'].each { |a| regions.keys.include? a } || site_excluded_regions&.include?(region['id'])
-    end
+  JSON.parse(File.read("#{data_dir}/all.json")).each do |name, website|
+    # Ignore if current region is excluded
+    next if website['regions']&.select { |r| r.start_with?('-') }.map! { |rc| rc.tr('-', '') }&.include?(region['id'])
+    # Ignore unless regions element includes current region or current region is int
+    next unless website['regions']&.reject { |r| r.start_with?('-') }.include?(key) || key.eql?('int')
 
     all[name] = website
     website['keywords'].each { |kw| used_categories.push kw }
@@ -50,11 +45,11 @@ Parallel.each(-> { regions.pop || Parallel::Stop }) do |region|
     file.write JSON.generate(categories.select { |cat| used_categories.include? cat['name'] })
   end
 
-  out_dir = "#{Dir.pwd}/_site/#{region['id']}"
-  puts "Building #{region['id']}..."
+  out_dir = "#{Dir.pwd}/_site/#{key}"
+  puts "Building #{key}..."
   # rubocop:disable Layout/LineLength
   puts `bundle exec jekyll build -s #{dest_dir} --config _config.yml,#{dest_dir}/_config_region.yml -d #{out_dir} --baseurl #{region['id']}`
   # rubocop:enable Layout/LineLength
-  puts "#{region['id']} built."
+  puts "#{key} built."
 end
 # rubocop:enable Metrics/BlockLength
