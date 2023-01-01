e6126d2726f1c35ad477896bdbb1ec5fd022281f
Carlgo11 <carlgo11@users.noreply.github.com>
2023-01-01 03:26:19 +0100

    Fix Algoia.rb handling of file deletions

--- scripts/Algolia.rb
+++ scripts/Algolia.rb
@@ -8,22 +8,35 @@ require 'dotenv/load'
 ALGOLIA_APP_ID = ENV['ALGOLIA_APP_ID']
 ALGOLIA_API_KEY = ENV['ALGOLIA_API_KEY']
 ALGOLIA_INDEX_NAME = ENV['ALGOLIA_INDEX_NAME']
-excludes = %w[notes documentation recovery]
 
+excludes = %w[notes documentation recovery]
 client = Algolia::Search::Client.create(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
 index = client.init_index(ALGOLIA_INDEX_NAME)
-diff = `git diff --name-only #{ARGV[0] || 'HEAD^'} entries/`
 updates = []
-diff.split("\n").each do |entry|
-  name, data = JSON.parse(File.read(entry)).first
-  puts "Updating #{name}"
-  data.merge!({ 'name' => name, 'objectID' => data['domain'] })
-  # Rename keys
-  data['2fa'] = data.delete 'tfa' if data.key? 'tfa'
-  data['category'] = data.delete 'keywords'
-  # Remove keys that shouldn't be searchable
-  data.reject! { |k, _| excludes.include? k }
-  updates.push data
+
+if ARGV[0].eql? '--all'
+  diff = Dir['entries/*/*.json']
+else
+  diff = `git diff --name-only #{ARGV[1] || 'HEAD^'} entries/`.split("\n")
 end
+
+diff.each do |entry|
+  if File.exist? entry
+    name, data = JSON.parse(File.read(entry)).first
+    puts "Updating #{name}"
+    data.merge!({ 'name' => name, 'objectID' => data['domain'] })
+    # Rename keys
+    data['2fa'] = data.delete 'tfa' if data.key? 'tfa'
+    data['category'] = data.delete 'keywords'
+    # Remove keys that shouldn't be searchable
+    data.reject! { |k, _| excludes.include? k }
+    updates.push data
+  else
+    domain = File.basename(entry, File.extname(entry))
+    puts "Removing #{domain}"
+    index.delete_object! domain
+  end
+end
+
 res = index.save_objects(updates)
 res.wait
