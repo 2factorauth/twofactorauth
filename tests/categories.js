bff9a7d883d650e925f9f2b1cd09e6d1ebe17bdb
H. Kamran <hkamran@hkamran.com>
2024-06-17 13:28:55 -0700

    Format tests (#8104)

--- tests/categories.js
+++ tests/categories.js
@@ -1,31 +1,32 @@
-const fs = require('fs').promises;
-const core = require('@actions/core');
+const fs = require("fs").promises;
+const core = require("@actions/core");
 
 async function main() {
   let errors = false;
   const files = process.argv.slice(2);
   const res = await fetch(
-    'https://raw.githubusercontent.com/2factorauth/frontend/master/data/categories.json',
+    "https://raw.githubusercontent.com/2factorauth/frontend/master/data/categories.json",
     {
-      'accept': 'application/json',
-      'user-agent': '2factorauth/twofactorauth +https://2fa.directory/bots',
-    });
+      accept: "application/json",
+      "user-agent": "2factorauth/twofactorauth +https://2fa.directory/bots",
+    },
+  );
 
-  if (!res.ok) throw new Error('Unable to fetch categories');
+  if (!res.ok) throw new Error("Unable to fetch categories");
 
   const data = await res.json();
   const allowed_categories = Object.keys(data);
 
   if (files) {
     for (const file of files) {
-      const data = await fs.readFile(file, 'utf8');
+      const data = await fs.readFile(file, "utf8");
       const json = await JSON.parse(data);
       const entry = json[Object.keys(json)[0]];
-      const {categories} = entry;
+      const { categories } = entry;
 
       for (const category of categories || []) {
         if (!allowed_categories.includes(category)) {
-          core.error(`${category} is not a valid category.`,{file});
+          core.error(`${category} is not a valid category.`, { file });
           errors = true;
         }
       }
--- tests/json.js
+++ tests/json.js
@@ -1,13 +1,13 @@
-const fs = require('fs').promises;
-const core = require('@actions/core');
-const Ajv = require('ajv');
-const addFormats = require('ajv-formats');
-const schema = require('./schema.json');
-const {basename} = require('node:path');
+const fs = require("fs").promises;
+const core = require("@actions/core");
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
+const schema = require("./schema.json");
+const { basename } = require("node:path");
 
-const ajv = new Ajv({strict: false, allErrors: true});
+const ajv = new Ajv({ strict: false, allErrors: true });
 addFormats(ajv);
-require('ajv-errors')(ajv);
+require("ajv-errors")(ajv);
 
 const validate = ajv.compile(schema);
 let errors = false;
@@ -26,16 +26,18 @@ function error(message, properties) {
 async function main() {
   const files = process.argv.slice(2);
 
-  await Promise.all(files.map(async (file) => {
-    try {
-      const json = await JSON.parse(await fs.readFile(file, 'utf8'));
-      const entry = json[Object.keys(json)[0]];
-      validateJSONSchema(file, json);
-      validateFileContents(file, entry);
-    } catch (e) {
-      error(`Failed to process ${file}: ${err.message}`, {file});
-    }
-  }));
+  await Promise.all(
+    files.map(async (file) => {
+      try {
+        const json = await JSON.parse(await fs.readFile(file, "utf8"));
+        const entry = json[Object.keys(json)[0]];
+        validateJSONSchema(file, json);
+        validateFileContents(file, entry);
+      } catch (e) {
+        error(`Failed to process ${file}: ${err.message}`, { file });
+      }
+    }),
+  );
 
   process.exit(+errors);
 }
@@ -51,12 +53,11 @@ function validateJSONSchema(file, json) {
   if (!valid) {
     errors = true;
     validate.errors.forEach((err) => {
-      const {message, instancePath, keyword: title} = err;
-      const instance = instancePath?.split('/');
+      const { message, instancePath, keyword: title } = err;
+      const instance = instancePath?.split("/");
       if (message)
-        error(`${instance[instance.length - 1]} ${message}`, {file, title});
-      else
-        error(err, {file});
+        error(`${instance[instance.length - 1]} ${message}`, { file, title });
+      else error(err, { file });
     });
   }
 }
@@ -71,27 +72,28 @@ function validateFileContents(file, entry) {
   const valid_name = `${entry.domain}.json`;
 
   if (basename(file) !== valid_name)
-    error(`File name should be ${valid_name}`, {file, title: 'File name'});
+    error(`File name should be ${valid_name}`, { file, title: "File name" });
 
   if (entry.url === `https://${entry.domain}`)
-    error(`Unnecessary url element defined.`, {file});
+    error(`Unnecessary url element defined.`, { file });
 
   if (entry.img === `${entry.domain}.svg`)
-    error(`Unnecessary img element defined.`, {file});
+    error(`Unnecessary img element defined.`, { file });
 
   if (file !== `entries/${entry.domain[0]}/${valid_name}`)
-    error(`Entry should be placed in entries/${entry.domain[0]}/`, {file});
+    error(`Entry should be placed in entries/${entry.domain[0]}/`, { file });
 
-  if (entry.tfa?.includes('custom-software') && !entry['custom-software'])
-    error('Missing custom-software element', {file});
+  if (entry.tfa?.includes("custom-software") && !entry["custom-software"])
+    error("Missing custom-software element", { file });
 
-  if (entry.tfa?.includes('custom-hardware') && !entry['custom-hardware'])
-    error('Missing custom-hardware element', {file});
+  if (entry.tfa?.includes("custom-hardware") && !entry["custom-hardware"])
+    error("Missing custom-hardware element", { file });
 
   if (entry.tfa && !entry.documentation)
     core.warning(
-      'No documentation set. Please provide screenshots in the pull request',
-      {file, title: 'Missing documentation'});
+      "No documentation set. Please provide screenshots in the pull request",
+      { file, title: "Missing documentation" },
+    );
 }
 
 module.exports = main();
