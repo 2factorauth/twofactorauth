ed6f7fe4f02f1aef821e597719e25c11572fb9d7
H. Kamran <hkamran@hkamran.com>
2024-07-12 17:26:07 -0700

    Minor updates to JavaScript scripts (#49)
    * Use `in` for checking key existence
    
    * Remove extraneous curly braces
    
    * Run Prettier

--- scripts/APIv3.js
+++ scripts/APIv3.js
@@ -1,97 +1,92 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
+const fs = require("fs");
+const path = require("path");
 
-let all = {}
-let tfa = {}
-let regions = {}
+let all = {};
+let tfa = {};
+let regions = {};
 
 // Read all JSON files from the 'entries' directory
-fs.readdirSync('entries').forEach((dir) => {
-  fs.readdirSync(path.join('entries', dir)).forEach((file) => {
-    if (file.endsWith('.json')) {
+fs.readdirSync("entries").forEach((dir) => {
+  fs.readdirSync(path.join("entries", dir)).forEach((file) => {
+    if (file.endsWith(".json")) {
       let data = JSON.parse(
-        fs.readFileSync(path.join('entries', dir, file), 'utf8')
-      )
-      let key = Object.keys(data)[0]
-      all[key] = data[key]
+        fs.readFileSync(path.join("entries", dir, file), "utf8"),
+      );
+      let key = Object.keys(data)[0];
+      all[key] = data[key];
     }
-  })
-})
+  });
+});
 
 // Process all entries
 Object.keys(all)
   .sort()
-  .forEach((key) => {
-    let value = all[key]
+  .forEach((entryName) => {
+    let entry = all[entryName];
 
     // Process tfa methods
-    if (value['tfa']) {
-      value['tfa'].forEach((method) => {
-        if (!tfa[method]) {
-          tfa[method] = {}
-        }
-        tfa[method][key] = value
-      })
+    if ("tfa" in entry) {
+      entry["tfa"].forEach((method) => {
+        if (!tfa[method]) tfa[method] = {};
+
+        tfa[method][key] = entry;
+      });
     }
 
     // Process regions
-    if (value['regions']) {
-      value['regions'].forEach((region) => {
-        if (region[0] !== '-') {
-          if (!regions[region]) {
-            regions[region] = { count: 0 }
-          }
-          regions[region]['count'] += 1
+    if ("regions" in entry) {
+      entry["regions"].forEach((region) => {
+        if (region[0] !== "-") {
+          if (!regions[region]) regions[region] = { count: 0 };
+
+          regions[region]["count"] += 1;
         }
-      })
+      });
     }
 
     // Rename 'categories' to 'keywords'
-    if (value['categories']) {
-      value['keywords'] = value['categories']
-      delete value['categories']
+    if ("categories" in entry) {
+      entry["keywords"] = entry["categories"];
+      delete entry["categories"];
     }
-  })
+  });
 
 // Write the all.json and tfa files
-const outputDir = 'api/v3'
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true })
-}
+const outputDir = "api/v3";
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir, { recursive: true });
 
-const writeJsonFile = (filename, data) => {
+const writeJsonFile = (filename, data) =>
   fs.writeFileSync(
     path.join(outputDir, filename),
-    JSON.stringify(data, null, 2)
-  )
-}
+    JSON.stringify(data, null, 2),
+  );
 
-writeJsonFile('all.json', { all })
+writeJsonFile("all.json", { all });
 
-Object.keys(tfa).forEach((method) => {
-  writeJsonFile(`${method}.json`, tfa[method])
-})
+Object.keys(tfa).forEach((method) =>
+  writeJsonFile(`${method}.json`, tfa[method]),
+);
 
 // Add the 'int' region
-regions['int'] = { count: Object.keys(all).length, selection: true }
+regions["int"] = { count: Object.keys(all).length, selection: true };
 
 // Write regions.json
 const sortedRegions = Object.entries(regions)
   .sort(([, a], [, b]) => b.count - a.count)
   .reduce((acc, [k, v]) => {
-    acc[k] = v
-    return acc
-  }, {})
-writeJsonFile('regions.json', sortedRegions)
+    acc[k] = v;
+    return acc;
+  }, {});
+writeJsonFile("regions.json", sortedRegions);
 
 // Write tfa.json
 const tfaEntries = Object.entries(all)
-  .filter(([, value]) => value.tfa)
+  .filter(([, entry]) => entry.tfa)
   .sort(([a], [b]) => a.toLowerCase().localeCompare(b.toLowerCase()))
   .reduce((acc, [k, v]) => {
-    acc[k] = v
-    return acc
-  }, {})
-writeJsonFile('tfa.json', tfaEntries)
+    acc[k] = v;
+    return acc;
+  }, {});
+writeJsonFile("tfa.json", tfaEntries);
--- scripts/Algolia.js
+++ scripts/Algolia.js
@@ -1,24 +1,26 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const algoliasearch = require('algoliasearch');
-const core = require('@actions/core');
-require('dotenv').config(); // Load environment variables from .env file
+const fs = require("fs");
+const path = require("path");
+const algoliasearch = require("algoliasearch");
+const core = require("@actions/core");
+require("dotenv").config(); // Load environment variables from .env file
 
 // List of keys to exclude from the search data
-const excludes = ['notes', 'documentation', 'recovery'];
+const excludes = ["notes", "documentation", "recovery"];
 
 // Initialize Algolia client and index using environment variables
-const client = algoliasearch(process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY);
+const client = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY,
+);
 const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME);
 
 // Function to process each file
 async function processFile(entry) {
   if (fs.existsSync(entry)) {
     // If the file exists, read and parse the JSON content
-    const fileContent = JSON.parse(fs.readFileSync(entry, 'utf8'));
+    const fileContent = JSON.parse(fs.readFileSync(entry, "utf8"));
     const [name, data] = Object.entries(fileContent)[0];
     core.info(`Updating ${name}`);
 
@@ -27,19 +29,19 @@ async function processFile(entry) {
     data.objectID = data.domain;
 
     // Rename keys if necessary
-    const {tfa, categories, ...rest} = data;
+    const { tfa, categories, ...rest } = data;
     const renamedData = {
       ...rest,
-      ...(tfa && {'2fa': tfa}),
-      ...(categories && {category: categories}),
+      ...(tfa && { "2fa": tfa }),
+      ...(categories && { category: categories }),
     };
 
     // Remove excluded keys from the data
-    excludes.forEach(exclude => delete renamedData[exclude]);
+    excludes.forEach((exclude) => delete renamedData[exclude]);
     return renamedData;
   } else {
     // If the file does not exist, remove the corresponding object from Algolia
-    const domain = path.basename(entry, '.json');
+    const domain = path.basename(entry, ".json");
     core.info(`Removing ${domain}`);
     await index.deleteObject(domain);
     return null;
@@ -54,12 +56,13 @@ async function main() {
 
   // Process each file in parallel
   const results = await Promise.allSettled(
-    files.map(async entry => processFile(entry)));
+    files.map(async (entry) => processFile(entry)),
+  );
 
-  results.forEach(result => {
-    if (result.status === 'fulfilled' && result.value) {
+  results.forEach((result) => {
+    if (result.status === "fulfilled" && result.value) {
       updates.push(result.value);
-    } else if (result.status === 'rejected') {
+    } else if (result.status === "rejected") {
       errors = true;
       core.error(`Failed to process a file: ${result.reason}`);
     }
@@ -69,16 +72,16 @@ async function main() {
   if (updates.length > 0) {
     try {
       await index.saveObjects(updates);
-      core.info('All updates saved');
+      core.info("All updates saved");
     } catch (err) {
-      core.error('Error saving updates:', err);
+      core.error("Error saving updates:", err);
       errors = true;
     }
   }
   process.exit(+errors);
 }
 
-main().catch(err => {
+main().catch((err) => {
   core.error(err);
   process.exit(1);
 });
